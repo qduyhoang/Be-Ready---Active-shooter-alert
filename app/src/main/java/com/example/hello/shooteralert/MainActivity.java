c852d9a65564ee62315009785fed40c35ad54882
Duy Hoang <qduyhoang@gmail.com>
2018-04-14 10:26:29 -0400

    Initial commit

---
+++ app/src/main/java/com/example/hello/shooteralert/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.hello.shooteralert;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+
+public class MainActivity extends AppCompatActivity {
+    public int RC_SIGN_IN;
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        //No call for super(). Bug on API Level > 11.
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        /*setContentView(R.layout.activity_main);*/
+        RC_SIGN_IN = 123;
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null) {
+            // already signed in
+        } else {
+            // not signed in
+        }
+
+// ...
+
+// Choose authentication providers
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.FacebookBuilder().build());
+
+// Create and launch sign-in intent
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // RC_SIGN_IN is the request code you passed into startActivityForResult(...) when starting the sign in flow.
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            // Successfully signed in
+            if (resultCode == RESULT_OK) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                Intent signedIn = new Intent(this, MapsActivity.class);
+                Log.d("successful", "onActivityResult: "+"Successful");
+                startActivity(signedIn);
+                finish();
+            } else {
+                // Sign in failed
+                if (response == null) {
+                    // User pressed back button
+                    return;
+                }
+
+                if (response.getError().getErrorCode() == ErrorCodes.NO_NETWORK) { ;
+                    return;
+                }
+
+                Log.e("Line 74", "Sign-in error: ", response.getError());
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/hello/shooteralert/SignedInActivity.java
@@ -0,0 +1,6 @@
+package com.example.hello.shooteralert;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class SignedInActivity extends AppCompatActivity {
+}
